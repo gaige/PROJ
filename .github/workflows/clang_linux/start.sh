0b8a039fc862d8c14db1dcdeec95907a68414c38
Gaige B Paulsen <github@gbp.gaige.net>
2023-05-05 05:44:49 -0400

    chore: update for clang 14 testing
    Testing clang14 because of the change in fp-contract default

--- .github/workflows/clang_linux/start.sh
+++ .github/workflows/clang_linux/start.sh
@@ -5,7 +5,7 @@ set -e
 apt-get update -y
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
     autoconf automake libtool make cmake ccache pkg-config python3-pip sqlite3 tar zip \
-    clang++-10 jq python3-clang-10 \
+    clang++-14 jq python3-clang-14 \
     libsqlite3-dev \
     libtiff-dev libwebp-dev libzstd-dev \
     libcurl4-openssl-dev libnghttp2-dev libidn2-dev librtmp-dev libssh-dev \
@@ -25,7 +25,7 @@ fi
 ccache -M 500M
 
 # -fno-use-cxa-atexit is needed to build with -coverage
-CC="clang-10" CXX="clang++-10" CFLAGS="-Werror -fsanitize=address -fno-use-cxa-atexit" CXXFLAGS="-Werror -fsanitize=address -fno-use-cxa-atexit" LDFLAGS="-fsanitize=address" ./travis/install.sh
+CC="clang-14" CXX="clang++-14" CFLAGS="-Werror -fsanitize=address -fno-use-cxa-atexit" CXXFLAGS="-Werror -fsanitize=address -fno-use-cxa-atexit" LDFLAGS="-fsanitize=address" ./travis/install.sh
 
 echo "Saving ccache..."
 rm -f "$WORK_DIR/ccache.tar.gz"
